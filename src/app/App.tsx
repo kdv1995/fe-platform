53d8babe98438c4c9d0a8be28e126aeb647dae1f
Danyil <tribeofdanel@gmail.com>
2025-04-26 18:40:07 +0300

    features/pages bootstraping
    1. Router is configured
    2. History page defined
    3. Index page defined
    4. Pdf page is defined

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import IndexPage from "../pages";
+import PdfHistoryPage from "../pages/pdf/history";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<IndexPage />} path="/" />
+        <Route path="pdf">
+          <Route path="history" element={<PdfHistoryPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
